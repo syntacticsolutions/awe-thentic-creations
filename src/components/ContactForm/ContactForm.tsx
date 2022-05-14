7a51655628d08e63dc148acf74f5503e2edd5916
Miguel Coder <syntacticsolutions@gmail.com>
2022-05-14 10:01:33 -0700

    Added recaptcha

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export const ContactForm = () => {
+  const [recaptchaVerified, setRecaptchaVerified] = useState(
+    process.env.NODE_ENV === "development"
+  );
+  const [messageSent, setMessageSent] = useState(false);
+  const [data, setData] = useState({});
+
+  const setInputValue = (key: string) => (e: any) => {
+    setData({ ...data, [key]: e.target.value });
+  };
+
+  async function sendText() {
+    if (recaptchaVerified) {
+      const response = await fetch(
+        "https://us-central1-directed-galaxy-221521.cloudfunctions.net/messaging-backend/sendMessage",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+        }
+      );
+      if (response.ok) {
+        setMessageSent(true);
+      }
+    }
+  }
+
+  async function onChange(value: any) {
+    const url =
+      "https://us-central1-directed-galaxy-221521.cloudfunctions.net/messaging-backend/verify";
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: value,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.responseCode === 0) {
+      setRecaptchaVerified(true);
+    }
+  }
   return (
     <div className="contact-form-container">
       <div className="form-description">
@@ -9,11 +55,43 @@ export const ContactForm = () => {
       </div>
       <div className="contact-form">
         <div className="flex">
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
+          <input
+            onChange={setInputValue("name")}
+            value={data.name}
+            type="text"
+            placeholder="Name"
+          />
+          <input
+            onChange={setInputValue("phone")}
+            value={data.phone}
+            type="text"
+            placeholder="Phone Number"
+          />
+          <input
+            onChange={setInputValue("email")}
+            value={data.email}
+            type="email"
+            placeholder="Email"
+          />
         </div>
-        <textarea placeholder="Message" />
-        <button type="submit">Get In Touch</button>
+        <textarea
+          onChange={setInputValue("message")}
+          value={data.message}
+          placeholder="Message"
+        />
+
+        <ReCAPTCHA
+          sitekey="6LcFxeofAAAAAJcRIQ1-uhuGa0fpH8rUGnKDPsu9"
+          onChange={onChange}
+          style={{ marginBottom: "20px" }}
+        />
+        <button
+          disabled={!recaptchaVerified || messageSent}
+          type="submit"
+          onClick={sendText}
+        >
+          Get In Touch
+        </button>
       </div>
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
+    <>
     <header id="qodef-page-header">
       <div id="qodef-page-header-inner">
         <div className="qodef-header-logo-wrapper">
@@ -421,5 +422,79 @@ export const Header = () => {
         </div>
       </div>
     </header>
+    <header id="qodef-page-mobile-header">
+<div id="qodef-page-mobile-header-inner">
+<a itemprop="url" class="qodef-mobile-header-logo-link" href="https://banquet.qodeinteractive.com/" rel="home">
+<img src="https://banquet.qodeinteractive.com/wp-content/themes/banquet/assets/img/logo.png" class="qodef-header-logo-image qodef--main" alt="logo main"> <img width="182" height="60" src="https://banquet.qodeinteractive.com/wp-content/uploads/2019/12/header-logo-light.png" class="qodef-header-logo-image qodef--dark" alt="logo dark" loading="lazy"><img width="182" height="60" src="https://banquet.qodeinteractive.com/wp-content/uploads/2019/12/header-logo-light.png" class="qodef-header-logo-image qodef--light" alt="logo light" loading="lazy"></a><a id="qodef-mobile-header-opener" href="#">
+<span class="qodef-lines">
+<span class="qodef-line qodef-line-1"></span>
+<span class="qodef-line qodef-line-2"></span>
+</span>
+</a> <nav id="qodef-mobile-header-navigation" class="qodef-m" role="navigation" aria-label="Mobile Menu">
+<div class="qodef-m-inner"><ul id="qodef-mobile-header-navigation-menu" class="qodef-content-grid"><li class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-23 qodef-menu-item--narrow"><a href="#"><span class="qodef-menu-item-inner">Home</span></a><span class="qodef-menu-arrow"></span>
+<div class="qodef-drop-down-second"><div class="qodef-drop-down-second-inner"><ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-15 current_page_item menu-item-155"><a href="https://banquet.qodeinteractive.com/"><span class="qodef-menu-item-inner">Wedding Home</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-275"><a href="https://banquet.qodeinteractive.com/special-events/"><span class="qodef-menu-item-inner">Special Events</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-363"><a href="https://banquet.qodeinteractive.com/corporate-party/"><span class="qodef-menu-item-inner">Corporate Party</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-913"><a href="https://banquet.qodeinteractive.com/landing/"><span class="qodef-menu-item-inner">Landing</span></a></li>
+</ul></div></div>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-24 qodef-menu-item--narrow"><a href="#"><span class="qodef-menu-item-inner">Pages</span></a><span class="qodef-menu-arrow"></span>
+<div class="qodef-drop-down-second"><div class="qodef-drop-down-second-inner"><ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-418"><a href="https://banquet.qodeinteractive.com/about-us/"><span class="qodef-menu-item-inner">About Us</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-387"><a href="https://banquet.qodeinteractive.com/about-me/"><span class="qodef-menu-item-inner">About Me</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-624"><a href="https://banquet.qodeinteractive.com/press-awards/"><span class="qodef-menu-item-inner">Press &amp; Awards</span></a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-365"><a href="https://banquet.qodeinteractive.com/404"><span class="qodef-menu-item-inner">404 Error Page</span></a></li>
+</ul></div></div>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-565 qodef-menu-item--narrow"><a href="#"><span class="qodef-menu-item-inner">Services</span></a><span class="qodef-menu-arrow"></span>
+<div class="qodef-drop-down-second"><div class="qodef-drop-down-second-inner"><ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-668"><a href="https://banquet.qodeinteractive.com/social-events/"><span class="qodef-menu-item-inner">Social Events</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-566"><a href="https://banquet.qodeinteractive.com/menus/"><span class="qodef-menu-item-inner">Menus</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-689"><a href="https://banquet.qodeinteractive.com/weddings/"><span class="qodef-menu-item-inner">Weddings</span></a></li>
+</ul></div></div>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-25 qodef-menu-item--narrow"><a href="#"><span class="qodef-menu-item-inner">Blog</span></a><span class="qodef-menu-arrow"></span>
+<div class="qodef-drop-down-second"><div class="qodef-drop-down-second-inner"><ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-862"><a href="https://banquet.qodeinteractive.com/blog-right-sidebar/"><span class="qodef-menu-item-inner">Right sidebar</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-861"><a href="https://banquet.qodeinteractive.com/blog-left-sidebar/"><span class="qodef-menu-item-inner">Left Sidebar</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-860"><a href="https://banquet.qodeinteractive.com/no-sidebar-blog/"><span class="qodef-menu-item-inner">No sidebar</span></a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-441"><a href="#"><span class="qodef-menu-item-inner">Post types</span></a><span class="qodef-menu-arrow"></span>
+<ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-1474"><a href="https://banquet.qodeinteractive.com/this-summer-in-tuscany-themed-wedding/"><span class="qodef-menu-item-inner">Standard post</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-827"><a href="https://banquet.qodeinteractive.com/momentos-weddings-events/"><span class="qodef-menu-item-inner">Gallery post</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-828"><a href="https://banquet.qodeinteractive.com/plan-it-design-it/"><span class="qodef-menu-item-inner">Link post</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-829"><a href="https://banquet.qodeinteractive.com/holiday-gift-guide/"><span class="qodef-menu-item-inner">Quote post</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-825"><a href="https://banquet.qodeinteractive.com/tips-for-planing-new-event/"><span class="qodef-menu-item-inner">Video post</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-826"><a href="https://banquet.qodeinteractive.com/glamorous-destination-events/"><span class="qodef-menu-item-inner">Audio post</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-1477"><a href="https://banquet.qodeinteractive.com/what-every-bride-should-know/"><span class="qodef-menu-item-inner">No sidebar</span></a></li>
+</ul>
+</li>
+</ul></div></div>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-26 qodef-menu-item--narrow"><a href="#"><span class="qodef-menu-item-inner">Shop</span></a><span class="qodef-menu-arrow"></span>
+<div class="qodef-drop-down-second"><div class="qodef-drop-down-second-inner"><ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-231"><a href="https://banquet.qodeinteractive.com/shop/"><span class="qodef-menu-item-inner">Shop List</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-product menu-item-233"><a href="https://banquet.qodeinteractive.com/product/beautiful-tableware/"><span class="qodef-menu-item-inner">Shop Single</span></a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-232"><a href="#"><span class="qodef-menu-item-inner">Shop Pages</span></a><span class="qodef-menu-arrow"></span>
+<ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-228"><a href="https://banquet.qodeinteractive.com/my-account/"><span class="qodef-menu-item-inner">My Account</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-230"><a href="https://banquet.qodeinteractive.com/cart/"><span class="qodef-menu-item-inner">Cart</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-229"><a href="https://banquet.qodeinteractive.com/checkout/"><span class="qodef-menu-item-inner">Checkout</span></a></li>
+</ul>
+</li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-234"><a href="#"><span class="qodef-menu-item-inner">Shop Layouts</span></a><span class="qodef-menu-arrow"></span>
+<ul class="sub-menu">
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-808"><a href="https://banquet.qodeinteractive.com/shop/3-columns-grid/"><span class="qodef-menu-item-inner">3 Columns Grid</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-806"><a href="https://banquet.qodeinteractive.com/shop/4-columns-grid/"><span class="qodef-menu-item-inner">4 Columns Grid</span></a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-804"><a href="https://banquet.qodeinteractive.com/shop/5-columns-grid/"><span class="qodef-menu-item-inner">5 Columns Grid</span></a></li>
+</ul>
+</li>
+</ul></div></div>
+</li>
+</ul></div> </nav>
+</div>
+</header>
+    </>
   );
 };
